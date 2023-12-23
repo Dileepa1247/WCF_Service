7f21bbf02d3006f696be0a122e5c55c3315a8fcd
DILEEPA H B <113028071+Dileepa1247@users.noreply.github.com>
2023-12-24 00:44:56 +0530

    Add files via upload

---
+++ CalculatorWCFService/CalculatorWCFService/bin/Debug/Publish/App_Code/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    public double Add(double n1, double n2)
+    {
+        return n1 + n2; 
+    }
+
+    public double Div(double n1, double n2)
+    {
+        return n1/n2;
+    }
+
+    public double Mul(double n1, double n2)
+    {
+        return n1 * n2; 
+    }
+
+    public double Sub(double n1, double n2)
+    {
+        return n1-n2;
+    }
+}
+
+	
